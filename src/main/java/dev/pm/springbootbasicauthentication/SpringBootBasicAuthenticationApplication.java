3a53accd6434a63382f8fc0ea6ca6099b3300d9c
Parth Makadiya <parth.makadiya@neptune-ubi.com>
2022-02-19 00:25:16 +0530

    initial commit

---
+++ src/main/java/dev/pm/springbootbasicauthentication/SpringBootBasicAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package dev.pm.springbootbasicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicAuthenticationApplication.class, args);
+	}
+
+}
